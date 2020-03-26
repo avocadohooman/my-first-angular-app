19c66b69d5ec7c7865594eec6247c8706fa0b218
gmolin <gmolin@student.hive.fi>
2020-03-26 13:05:06 +0200

    finishing seciton 6: can switch between recipes and shopping list, and can add new items to shopping list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+@Output() selectEvent = new EventEmitter<string>();
+
+
+selector(feature: string){
+  this.selectEvent.emit(feature);
+}
 
 }
